8f05f337be8b9fb65dcdb445f7e3dc3fb47a5995
SalauddinAmir <mdsalauddinahmed.amir@gmail.com>
2023-05-20 01:05:51 +0600

    category shop done

--- src/Pages/Home/CategoryToys/CategoryToys.jsx
+++ src/Pages/Home/CategoryToys/CategoryToys.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import category from './Category.css'
+import Toys from './Toys';
 
 const CategoryToys = () => {
+    const [categoryToys,setCategoryToys]=useState([])
     const [activeTab, setActiveTab] = useState("Collectible_Dolls");
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/allToysByCategory/${activeTab}`)
+          .then((res) => res.json())
+          .then((result) => {
+            setCategoryToys(result);
+          });
+      }, [activeTab]);
+      console.log(categoryToys)
+
 
     const handleTabClick = (tabName) => {
         setActiveTab(tabName);
       };
     return (
-        <div>
-        <div className=" text-center h-48">
+       <div className='my-5'>
+         <div className='max-w-7xl mx-auto'>
+        <div className=" text-center ">
         <div className="text-center w-100 mx-auto">
           <div className="tabs  flex justify-center items-center">
             <div
@@ -38,9 +51,15 @@ const CategoryToys = () => {
             </div>
           </div>
         </div>
+        <div className='grid grid-col-1 lg:grid-cols-2  pt-16 pb-16'>
+            {
+                categoryToys?.map(toy=><Toys toy={toy} key={toy._id}></Toys>)
+            }
+        </div>
       </div>
             
         </div>
+       </div>
     );
 };
 
---
+++ src/Pages/Home/CategoryToys/Toys.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Toys = ({toy}) => {
+    // console.log(toy)
+    const {photo,_id,price,rating,toyName}=toy
+    return (
+        <div className="card card-compact w-96 h-96 bg-base-100 shadow-xl">
+        <figure><img src= {photo} alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">{toyName}</h2>
+          <div className='flex'>
+            <p>{rating}</p>
+           <p>Price: ${price}</p>
+
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Toys;
